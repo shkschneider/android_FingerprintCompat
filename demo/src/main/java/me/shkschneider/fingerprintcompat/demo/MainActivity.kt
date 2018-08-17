727fabe27f93bdee339d85aaf82fb86a5d325823
shkschneider <shk.schneider@gmail.com>
2018-08-17 17:41:18 +0200

    Initial commit

---
+++ demo/src/main/java/me/shkschneider/fingerprintcompat/demo/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.shkschneider.fingerprintcompat.demo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
